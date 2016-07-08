2e641437b2369268fdc4562d5884278ffdb9e73f
gitaaronc <aaronc@shaw.ca>
2016-07-07 19:13:29 -0700

    Added basic config.hpp file for global settings

---
+++ include/config.hpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   config.hpp
+ * Author: aaronc
+ *
+ * Created on July 7, 2016, 7:07 PM
+ */
+
+#ifndef CONFIG_HPP
+#define	CONFIG_HPP
+
+namespace ace {
+namespace config {
+static std::string serial_port = "/dev/ttyUSB0";
+static int baud_rate = 19200;
+}
+
+
+#endif	/* CONFIG_HPP */
+
